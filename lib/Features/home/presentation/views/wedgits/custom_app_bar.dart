ded72a556ac766437db2fc13bccce7adf7ba717a
ma7moud332 <119896210+ma7moud332@users.noreply.github.com>
2024-12-19 19:27:00 +0200

    Add featured list view to home

--- lib/Features/home/presentation/views/wedgits/custom_app_bar.dart
+++ lib/Features/home/presentation/views/wedgits/custom_app_bar.dart
@@ -10,7 +10,7 @@ class CustomAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 24,vertical: 50),
+      padding: const EdgeInsets.only(left: 24,right: 24,top: 40,bottom: 40),
       child: Row(
         children: [
           Image.asset(AssetsData.logo,
--- lib/Features/home/presentation/views/wedgits/custom_list_view_item.dart
+++ lib/Features/home/presentation/views/wedgits/custom_list_view_item.dart
@@ -3,25 +3,22 @@ import 'package:flutter/material.dart';
 
 import '../../../../../core/utils/assets.dart';
 
-class CustomListViewItem extends StatelessWidget {
-  const CustomListViewItem({super.key});
+class FeaturedListViewItem extends StatelessWidget {
+  const FeaturedListViewItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * .3,
-      child: AspectRatio(
-        aspectRatio: 2.7/4,
-        child: Container(
-          decoration:  BoxDecoration(
-              borderRadius: BorderRadius.circular(22),
-              color: Colors.white,
-              image: const DecorationImage(
-                fit: BoxFit.fill,
-                image: AssetImage(
-                    AssetsData.testImage),
-              )
-          ),
+    return AspectRatio(
+      aspectRatio: 2.7/4,
+      child: Container(
+        decoration:  BoxDecoration(
+            borderRadius: BorderRadius.circular(22),
+            color: Colors.white,
+            image: const DecorationImage(
+              fit: BoxFit.fill,
+              image: AssetImage(
+                  AssetsData.testImage),
+            )
         ),
       ),
     );
