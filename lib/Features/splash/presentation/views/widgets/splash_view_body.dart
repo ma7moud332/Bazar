0704526ef62759685d54013759f1c9589a79b0cf
ma7moud332 <119896210+ma7moud332@users.noreply.github.com>
2024-12-19 17:43:39 +0200

    Refactor init state & navigate to home

--- lib/Features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/Features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,7 +1,11 @@
+import 'package:book_app/Features/home/presentation/views/home_view.dart';
 import 'package:book_app/Features/splash/presentation/views/widgets/sliding_text.dart';
 import 'package:book_app/core/utils/assets.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import '../../../../../constants.dart';
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
@@ -17,22 +21,13 @@ class _SplashViewBodyState extends State<SplashViewBody> with SingleTickerProvid
 
   @override
   void initState() {
-
     super.initState();
-
-    animationController = AnimationController(
-      vsync: this ,
-      duration: const Duration(seconds: 1),
-    );
-
-    slidingAnimation = Tween<Offset>(begin: const Offset(0, 2) , end: Offset.zero).animate(animationController);
-
-    animationController.forward();
+    initSlidingAnimation();
+    navigateToHome();
   }
 
   @override
   void dispose() {
-
     super.dispose();
     animationController.dispose();
   }
@@ -51,4 +46,25 @@ class _SplashViewBodyState extends State<SplashViewBody> with SingleTickerProvid
       ],
     );
   }
+
+
+  void initSlidingAnimation() {
+    animationController = AnimationController(
+      vsync: this ,
+      duration: const Duration(seconds: 1),
+    );
+
+    slidingAnimation = Tween<Offset>(begin: const Offset(0, 2) , end: Offset.zero).animate(animationController);
+
+    animationController.forward();
+  }
+
+  void navigateToHome() {
+    Future.delayed(const Duration(seconds: 3),(){
+      Get.to(()=> const HomeView(),
+        transition: Transition.fade ,
+        duration: KTransitionDuration,);
+    });
+  }
+
 }
--- lib/constants.dart
+++ lib/constants.dart
@@ -2,3 +2,5 @@
 import 'dart:ui';
 
 const KPrimaryColor = Color(0xff100B20);
+
+const KTransitionDuration = Duration(milliseconds: 250);
