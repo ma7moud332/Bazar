16b95d00d0d518ec16ac49f03b920eac4a72d358
ma7moud332 <119896210+ma7moud332@users.noreply.github.com>
2024-12-24 00:12:39 +0200

    create featured books cubit state

--- lib/Features/home/data/repository/home_repo_impl.dart
+++ lib/Features/home/data/repository/home_repo_impl.dart
@@ -20,9 +20,7 @@ class HomeRepoImpl implements HomeRepo {
      for(var item in data['items']) {
        books.add(BookModel.fromJson(item));
      }
-
      return right(books);
-
    } catch (e) {
      if(e is DioException){
        return left(ServerFailure.fromDioError(e),
@@ -32,12 +30,27 @@ class HomeRepoImpl implements HomeRepo {
      ),
      );
    }
-
   }
-
   @override
-  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() async {
 
-    throw UnimplementedError();
+    try {
+      var data = await apiService.get(
+          endPoint: 'volumes?Filtering=free-ebooks&q=subject:programming');
+      List<BookModel> books = [];
+
+      for(var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } catch (e) {
+      if(e is DioException){
+        return left(ServerFailure.fromDioError(e),
+        );
+      }
+      return left(ServerFailure(e.toString(),
+      ),
+      );
+    }
   }
 }
