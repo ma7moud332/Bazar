be0586a5aa4c4fcc9c844fae195fe3219cd50498
ma7moud332 <119896210+ma7moud332@users.noreply.github.com>
2024-12-23 19:24:42 +0200

    create service failure from dioError

--- lib/Features/home/data/repository/home_repo_impl.dart
+++ lib/Features/home/data/repository/home_repo_impl.dart
@@ -1,13 +1,38 @@
 import 'package:book_app/Features/home/data/models/book_model/book_model.dart';
 import 'package:book_app/Features/home/data/repository/home_repo.dart';
 import 'package:book_app/core/errors/failures.dart';
+import 'package:book_app/core/utils/api_service.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 class HomeRepoImpl implements HomeRepo {
-  @override
-  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
+  final ApiService apiService;
 
-    throw UnimplementedError();
+  HomeRepoImpl(this.apiService);
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchNewestBooks() async{
+
+   try {
+     var data = await apiService.get(
+         endPoint: 'volumes?Filtering=free-ebooks&q=subject:programming&Sorting=newest');
+     List<BookModel> books = [];
+
+     for(var item in data['items']) {
+       books.add(BookModel.fromJson(item));
+     }
+
+     return right(books);
+
+   } catch (e) {
+     if(e is DioError){
+       return left(ServerFailure.fromDioError(e),
+       );
+     }
+     return left(ServerFailure(e.toString(),
+     ),
+     );
+   }
+
   }
 
   @override
--- lib/core/errors/failures.dart
+++ lib/core/errors/failures.dart
@@ -1,3 +1,54 @@
-abstract class Failure{}
+import 'package:dio/dio.dart';
 
-class ServerFailure extends Failure{}
+abstract class Failure{
+
+  final String errMessage;
+
+  Failure(this.errMessage);
+}
+
+class ServerFailure extends Failure{
+  ServerFailure(super.errMessage);
+
+  factory ServerFailure.fromDioError(DioError dioError){
+
+    switch(dioError.type){
+      case DioExceptionType.connectionTimeout:
+        return ServerFailure('Connection Timeout with ApiServer');
+      case DioExceptionType.sendTimeout:
+        return ServerFailure('Send Timeout with ApiServer');
+      case DioExceptionType.receiveTimeout:
+        return ServerFailure('Receive Timeout with ApiServer');
+      case DioExceptionType.badCertificate:
+        return ServerFailure('BadCertificate');
+      case DioExceptionType.badResponse:
+        return ServerFailure.fromResponse(dioError.response!.statusCode!, dioError.response!.data);
+      case DioExceptionType.cancel:
+        return ServerFailure('Request to ApiServer was canceled');
+      case DioExceptionType.connectionError:
+        return ServerFailure('Connection Error');
+      case DioExceptionType.unknown:
+        if(dioError.message!.contains('SocketException')) {
+          return ServerFailure('No Internet Connection');
+        }
+        return ServerFailure('Unexpected Error , Please Try again!');
+      default:
+        return ServerFailure('Opps There was an error,Please try again');
+    }
+  }
+
+  factory ServerFailure.fromResponse(int statusCode , dynamic response){
+    if(statusCode == 400 || statusCode == 401 || statusCode == 403){
+      return ServerFailure(response['error']['message']);
+    }
+    else if (statusCode == 404){
+      return ServerFailure('Your request not found, please try again!');
+    }
+    else if (statusCode == 500 ){
+      return ServerFailure('Internal Server Error , please try again!');
+    }
+    else {
+      return ServerFailure('Opps There was an error,Please try again');
+    }
+  }
+}
