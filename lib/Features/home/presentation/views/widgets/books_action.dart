0b2d98dc69b04f5025ab36f6b0e2cc694092404a
ma7moud332 <119896210+ma7moud332@users.noreply.github.com>
2024-12-24 19:32:31 +0200

    book search view done

--- lib/Features/home/presentation/views/widgets/books_action.dart
+++ lib/Features/home/presentation/views/widgets/books_action.dart
@@ -1,7 +1,6 @@
 import 'package:book_app/Features/home/data/models/book_model/book_model.dart';
 import 'package:book_app/core/utils/functions/launch_url.dart';
 import 'package:flutter/material.dart';
-
 import '../../../../../core/widgets/custom_button.dart';
 
 class BookAction extends StatelessWidget {
